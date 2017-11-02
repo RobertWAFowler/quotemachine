889c5594d5156e0d0cb2902a93c6f8407d7b0de0
Robert Fowler <rfowler@localhost.localdomain>
2017-11-02 09:33:01 +0200

    First working version.

--- public_html/js/myJS.js
+++ public_html/js/myJS.js
@@ -4,17 +4,31 @@
  * and open the template in the editor.
  */
 var myQuotes = [{"quote": "This is a quote", "author": "Joe Blogs"},
-    {"quote": "adgfgsdf", "author": "srgwegwt"},
+    {"quote": "adgffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffgsdf", "author": "srgasfdasfasdfasdssssssssssssssssssssssssssssssssssssssssssssssssssssssdsssssssssssssssssssssssssssssssssssssssssssssssssssssssssswegwt"},
     {"quote": "dsfgsdfg", "author": "btrtwb"},
     {"quote": "sdfgsdf", "author": "btrwtw"},
     {"quote": "gfdgsdf", "author": "btrwb"},
     {"quote": "dfsgsfdg", "author": "trbwt"}];
 
+var quote = myQuotes[Math.floor(Math.random() * myQuotes.length)];
 
 
 function getRandomQuote() {
-    var quote = myQuotes[Math.floor(Math.random()*myQuotes.length)];
+    quote = myQuotes[Math.floor(Math.random() * myQuotes.length)];
     document.getElementById("myQuote").innerHTML = quote.quote;
     document.getElementById("myAuthor").innerHTML = quote.author;
-    return quote;              
+    return quote;
 }
+
+function tweetQuote() {
+
+    var textToTweet = quote.quote + " - " + quote.author;
+    if (textToTweet.length > 140) {
+        alert('Tweet should be less than 140 Chars');
+    } else {
+        var twtLink = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(textToTweet);
+        window.open(twtLink, '_blank');
+    }
+}
+
+
